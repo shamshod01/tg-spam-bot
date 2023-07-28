6dd505a76398d55ad9ef3201a972ecdb161c062a
shamshod01 <realshamshod01@gmail.com>
2023-07-28 17:44:38 +0900

    done

--- bot.js
+++ bot.js
@@ -3,19 +3,23 @@ const TelegramApi = require('node-telegram-bot-api')
 const { StringSession } = require("telegram/sessions");
 const cron = require('node-cron');
 const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const intialSession = require('./session.json')
-let SPAM = 'POST HERE'
-const ADMIN = []
-let LATITUDE = 37.468766
-let LONGITUDE = 126.936944
+require('dotenv').config()
+
+const PHOTO = 'file.jpg'
+let SPAM = 'Hello Everyone!'
+let LATITUDE = 25.254857//37.468766
+let LONGITUDE = 55.329514//126.936944
 let INTERVAL = 1
-let API_ID = process.env.API_ID
+let API_ID = Number(process.env.API_ID)
 let API_HASH = process.env.API_HASH
 let SESSION_STRING = intialSession.sessionString
 const BOT_TOKEN = process.env.BOT_TOKEN
-const LOGIN = 'ADMIN'
-const PASSWORD = '123'
-let CURRENT_USER = intialSession.phone
+const LOGIN = process.env.LOGIN
+const PASSWORD = process.env.PASSWORD
 
 
 
@@ -30,98 +34,77 @@ let CANDIDATE_DATA = {
 const bot = new TelegramApi(BOT_TOKEN, { polling: true })
 bot.setMyCommands([
     { command: '/start', description: 'restart' },
-    { command: '/check_msg', description: 'what post is been sending' },
     { command: '/check_int', description: 'how often posts are been sending' },
     { command: '/check_lct', description: 'current location' },
-    { command: '/sender', description: 'who sends posts' },
-    { command: '/message', description: 'update message' },
-    { command: '/interval', description: 'update interval' },    
+    { command: '/check_sndr', description: 'who sends posts' },
+    { command: '/sender', description: "update sender"},
 ])
 const sesssion = new StringSession(SESSION_STRING)
-const client = new TelegramClient(sesssion, API_ID, API_HASH, {});
+let client = new TelegramClient(sesssion, API_ID, API_HASH, {});
 
-async function getChats(){
+async function getChats() {
+    console.log(LONGITUDE)
+    console.log(LATITUDE)
     const result = await client.invoke(
         new Api.contacts.GetLocated({
             geoPoint: new Api.InputGeoPoint({
                 lat: LATITUDE,
                 long: LONGITUDE,
-                accuracyRadius: 43,
+                accuracyRadius: 4300,
             }),
             selfExpires: 43,
         })
     );
-
     return result.chats
 }
 
-module.exports.runBot = async ()  => {
+module.exports.runBot = async () => {
 
 
-
+    await client.connect()
 
 
     bot.on('message', async msg => {
         const text = msg.text ? msg.text : ''
         const chatId = msg.chat.id
+
         console.log(msg)
         if (text === '/start') {
-            return bot.sendSticker(chatId, 'CAACAgIAAxkBAAOLY4BuKYi5bkgILFnWv4n7gvgnpSEAAt0gAAKmUghIdWpUJizpPg0rBA')
-
+            return bot.sendSticker(chatId, 'CAACAgIAAxkBAAICeGTDbLZvdR46W50TsUyZ3v2Hf3jQAAL7BQAClvoSBZdb7eV44WgWLwQ')
         }
         if (text.includes('login')) {
             return await registerUser(msg)
+        }
+        if (chatId !== LOGIN_CANDIADATE) {
+            return bot.sendMessage(chatId, "It is telegram bot for massive sending Adds to nearby channels. text @manjeom_com if you need it.")
         }
         if (msg.location && chatId == LOGIN_CANDIADATE) {
             LATITUDE = msg.location.latitude
             LONGITUDE = msg.location.longitude
             return bot.sendMessage(chatId, "Location has been updated successfully")
         }
-        if (text.includes('message')) {
-            return await setMessage();
+        if (msg.photo && msg.photo[0]) {
+            SPAM = msg.text || msg.caption
+            const fileId = msg.photo[msg.photo.length-1].file_id;
+            await handlePhoto(fileId)
+            return bot.sendMessage(LOGIN_CANDIADATE, "Message was updated successfully")
         }
         if (text.includes('interval')) {
-            return await setMessageInterval();
+            let interval = parseDataFromString(text, 'interval')
+            if(!Number(interval) || Number(interval) > 23 || Number(interval) < 1) {
+                return bot.sendMessage(LOGIN_CANDIADATE, "Send only number between 1~23")    
+             }
+             INTERVAL = Number(interval);
+             scheduleCronJob(interval);
+            return bot.sendMessage(LOGIN_CANDIADATE, "Interval was updated successfully")
         }
-        if (chatId === LOGIN_CANDIADATE) {
-            return handleGetters(text);
+        if(text.includes('sender')) {
+            return await newSender()
         }
-
-        return bot.sendMessage(chatId, "It is telegram bot for massive sending Adds to nearby channels. text @manjeom_com if you need it.")
-
+        return handleGetters(text);
     })
 }
 
-
-
-async function setMessage() {
-    await bot.sendMessage(LOGIN_CANDIADATE, `Please enter your message`);
-    let done = false;
-    bot.on('message', async msg => {
-        if (msg.chat.id != LOGIN_CANDIADATE) return;
-        await client.connect()
-        const result = await client.invoke(
-            new Api.messages.ForwardMessages({
-              fromPeer: LOGIN_CANDIADATE,
-              id: msg.message_id,
-              randomId: [BigInt("-4156887774564")],
-              toPeer: msg.chat.id,
-              dropAuthor: true,
-            })
-          );
-          console.log(result); // prints the result
-        done = true;
-    })
-
-    return new Promise((resolve, reject) => {
-        const intervalId = setInterval(() => {
-            if (done) {
-                clearInterval(intervalId);
-                resolve(bot.sendMessage(LOGIN_CANDIADATE, "Message was updated successfully"));
-            }
-        }, 1000); // Check every 1 second (1000 milliseconds)
-    });
-}
 // Function that resolves a promise when the global variable is not null
 async function getUserData(data) {
     await bot.sendMessage(LOGIN_CANDIADATE, `Please enter your ${data}`);
@@ -141,7 +124,6 @@ async function getUserData(data) {
             CANDIDATE_DATA.password = parseDataFromString(text, 'password')
         }
     })
-    //  const result = await res(data);
     return new Promise((resolve, reject) => {
         const intervalId = setInterval(() => {
             if (CANDIDATE_DATA[data] !== null) {
@@ -151,48 +133,29 @@ async function getUserData(data) {
         }, 1000); // Check every 1 second (1000 milliseconds)
     });
 }
-
-async function setMessageInterval() {
-    await bot.sendMessage(LOGIN_CANDIADATE, `Please enter interval of messages in hours`);
-    let done = false;
-    bot.on('message', async msg => {
-        if (msg.chat.id != LOGIN_CANDIADATE) return;
-        INTERVAL = Number(msg.text);
-        done = true;
-    })
-
-    return new Promise((resolve, reject) => {
-        const intervalId = setInterval(() => {
-            if (done) {
-                clearInterval(intervalId);
-                scheduleCronJob(INTERVAL);
-                resolve(bot.sendMessage(LOGIN_CANDIADATE, "Interval was updated successfully"));
-            }
-        }, 1000); // Check every 1 second (1000 milliseconds)
-    });
-}
-
 async function handleGetters(text) {
-
-    if (text === '/check_msg') {
-        return bot.sendMessage(LOGIN_CANDIADATE, SPAM)
-    }
-    else if (text === '/check_int') {
+    if (text === '/check_int') {
         return bot.sendMessage(LOGIN_CANDIADATE, INTERVAL)
     }
     else if (text === '/check_lct') {
         return bot.sendMessage(LOGIN_CANDIADATE, `latitude: ${LATITUDE} longitude: ${LONGITUDE}`)
     }
-    else if (text === '/sender') {
-        return bot.sendMessage(LOGIN_CANDIADATE, CURRENT_USER);
+    else if (text === '/check_sndr') {
+        let me = await client.getMe()
+        return bot.sendMessage(LOGIN_CANDIADATE, me.phone);
     }
 }
-
 async function registerUser(msg) {
     if (LOGIN == parseDataFromString(msg.text, 'login') && PASSWORD == parseDataFromString(msg.text, 'password')) {
         LOGIN_CANDIADATE = msg.chat.id
+        return bot.sendMessage(msg.chat.id, "Welcome Boss!")
     }
-
+    return bot.sendMessage(msg.chat.id, "It is telegram bot for massive sending Adds to nearby channels. text @manjeom_com if you need it.")
+}
+async function newSender() {
+    await client.disconnect()
+    const sesssion = new StringSession('')
+    client = new TelegramClient(sesssion, API_ID, API_HASH, {});
     await client.start({
         phoneNumber: () => getUserData('phone'),
         password: () => getUserData("password"),
@@ -200,16 +163,14 @@ async function registerUser(msg) {
         onError: (err) => console.log(err),
     });
     SESSION_STRING = String(client.session.save())
-    removeAndCreateSessionFile(SESSION_STRING, CANDIDATE_DATA.phone);
     console.log(SESSION_STRING);
-    CURRENT_USER = CANDIDATE_DATA.phone;
+    removeAndCreateSessionFile(SESSION_STRING);
     CANDIDATE_DATA = {
         phone: null,
         password: null,
         recievedCode: null
     }
 }
-
 function parseDataFromString(inputString, dataName) {
     const regex = new RegExp(`(?<=${dataName}:\\s*)[^,]+`);
     const match = inputString.match(regex);
@@ -222,55 +183,95 @@ function parseDataFromString(inputString, dataName) {
     }
 }
 
-
 let scheduledJob; // Variable to store the scheduled cron job
 
 // Function to be called by cron
 async function taskFunction() {
 
-    await client.connect()
-    let chats  = await getChats()
-    client.floodSleepThreshold = 300; // sleeps if the wait is lower than 300 seconds
+   // await client.connect()
+    let chats = await getChats()
+
+    client.floodSleepThreshold = 500; // sleeps if the wait is lower than 300 seconds
     console.log(chats)
-    for(const i of chats) {
+    for (const i of chats) {
         const result = await client.invoke(
             new Api.channels.JoinChannel({
-              channel: i.id,
+                channel: i.id,
             })
-          );
+        );
         console.log(result);
-        const msgSend = await client.sendMessage(i.id, { message: SPAM });
-        console.log(msgSend);
-
+        await sendPost(i.id);
     }
     console.log("end of sending")
-  console.log('Task function called!');
+    console.log('Task function called!');
+}
+
+async function sendPost(chatId) {
+    let post = {message: SPAM}
+    if (fs.existsSync(PHOTO)) {
+        post.file = `./${PHOTO}`
+    }
+    const msgSend = await client.sendMessage(chatId, post);
+
+    console.log(msgSend);
 }
 
 // Cron job function that schedules the taskFunction based on the Interval
 function scheduleCronJob(interval) {
-  const cronExpression = `*/${interval} * * * *`; // Cron expression for the Interval in minutes
-  // Clear the previous cron job before scheduling the new one
-  if (scheduledJob) {
-    scheduledJob.stop();
-  }
+    const cronExpression = `*/${interval} * * * *`; // Cron expression for the Interval in minutes
+    // Clear the previous cron job before scheduling the new one
+    if (scheduledJob) {
+        scheduledJob.stop();
+    }
 
-  scheduledJob = cron.schedule(cronExpression, taskFunction);
-  console.log(`Task scheduled to run every ${interval} minutes.`);
+    scheduledJob = cron.schedule(cronExpression, taskFunction);
+    console.log(`Task scheduled to run every ${interval} minutes.`);
 }
 
-function removeAndCreateSessionFile(s, p) {
+function removeAndCreateSessionFile(s) {
     const fileName = 'session.json';
-    const textToWrite = JSON.stringify({sessionString: s, phone: p})
-  
+    const textToWrite = JSON.stringify({ sessionString: s })
+
     // Check if the file exists
-    if (fs.existsSync(fileName)) {
-      // Remove the existing file
-      fs.unlinkSync(fileName);
-      console.log(`'${fileName}' removed successfully.`);
-    }
-  
+    removeFile(fileName);
+
     // Create a new file and write some text into it
     fs.writeFileSync(fileName, textToWrite);
     console.log(`'${fileName}' created successfully with initial text.`);
-  }
+}
+
+function removeFile(fileName) {
+     // Check if the file exists
+     if (fs.existsSync(fileName)) {
+        // Remove the existing file
+        fs.unlinkSync(fileName);
+        console.log(`'${fileName}' removed successfully.`);
+    }
+}
+
+// this is used to download the file from the link
+const download = (url, path, callback) => {
+    request.head(url, (err, res, body) => {
+    request(url).pipe(fs.createWriteStream(path)).on('close', callback);
+  });
+};
+
+async function handlePhoto(fileId) {
+    removeFile(PHOTO)
+    const res = await fetch(
+        `https://api.telegram.org/bot${BOT_TOKEN}/getFile?file_id=${fileId}`
+      );
+      // extract the file path
+      const res2 = await res.json();
+      const filePath = res2.result.file_path;
+  
+      // now that we've "file path" we can generate the download link
+      const downloadURL = 
+        `https://api.telegram.org/file/bot${BOT_TOKEN}/${filePath}`;
+  
+      // download the file (in this case it's an image)
+      download(downloadURL, path.join(__dirname, PHOTO), () =>
+        console.log('Done!')
+        
+       );
+}
