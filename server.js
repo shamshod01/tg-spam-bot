041095a88eb0f20071498a7827138ab496beca5e
shamshod01 <realshamshod01@gmail.com>
2023-08-10 21:12:02 +0900

    update: get port from env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { runBot, getChats, sendSpam } = require('./bot');
 const app = express();
-const port = 4000//process.env.PORT;
+const port = process.env.PORT;
 
 runBot();
 
